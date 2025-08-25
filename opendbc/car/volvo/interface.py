1b4b6dd93d215d7013c4520189f28a825af0ca20
Paper1 <paper1@outlook.pt>
2025-08-25 07:17:53 +0100

    WIP

--- opendbc/car/volvo/interface.py
+++ opendbc/car/volvo/interface.py
@@ -14,9 +14,10 @@ class CarInterface(CarInterfaceBase):
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
     ret.brand = 'volvo'
 
-    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volvo)]
+    #ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volvo)]
+    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.noOutput)]
 
-    ret.dashcamOnly = False
+    ret.dashcamOnly = True
 
     ret.steerActuatorDelay = 0.3
     ret.steerLimitTimer = 0.1
--- opendbc/safety/modes/volvo.h
+++ opendbc/safety/modes/volvo.h
@@ -16,8 +16,6 @@
 #define VOLVO_PSCM_BUS    2U  // PSCM bus (BCM2, SAS, EGSM, where LCA is sent to)
 
 static void volvo_rx_hook(const CANPacket_t *msg) {
-  controls_allowed = true;
-  return;
   // Basic vehicle state monitoring - very relaxed implementation
 
   // VCU1 bus (bus 0) messages
@@ -48,7 +46,7 @@ static void volvo_rx_hook(const CANPacket_t *msg) {
 
       // Signal: CRUISE_OR_PILOT_ASSIST_ENGAGED (also on PSCM bus)
       bool cruise_engaged = (msg->data[1] >> 4) & 1U;
-      controls_allowed = cruise_engaged && !brake_pressed;
+      controls_allowed = cruise_engaged;
     }
 
     // Update steering angle from SAS
@@ -105,7 +103,20 @@ static safety_config volvo_init(uint16_t param) {
 
   // Define RX checks - minimal monitoring for basic safety
   static RxCheck volvo_rx_checks[] = {
+    // Gear position - from VCU1 bus (bus 0)
+    {.msg = {{VOLVO_GEAR_POSITION, VOLVO_VCU1_BUS, 8, 20U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},
 
+    // Vehicle speed - required for basic safety (on PSCM bus)
+    {.msg = {{VOLVO_BCM2_SPEED, VOLVO_PSCM_BUS, 8, 20U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},
+
+    // Brake pedal and cruise state - required for safety (on PSCM bus)
+    {.msg = {{VOLVO_BCM2, VOLVO_PSCM_BUS, 8, 20U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},
+
+    // Steering angle - required for lateral control (on PSCM bus)
+    {.msg = {{VOLVO_SAS, VOLVO_PSCM_BUS, 8, 100U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},
+
+    // Driver steering input - required for override detection (on PSCM bus)
+    {.msg = {{VOLVO_PSCM, VOLVO_PSCM_BUS, 8, 100U, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true}, { 0 }, { 0 }}},
   };
 
   return BUILD_SAFETY_CFG(volvo_rx_checks, VOLVO_TX_MSGS);
